ed762deb1e1e6f2b2481eed967c53b233db40d49
Darius Bowser <134982506+DariusBowser@users.noreply.github.com>
2024-01-22 11:12:22 -0500

    login setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,54 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+import "./style.scss";
+
+const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/write", element: <Write /> },
+      { path: "/post/:id", element: <Single /> },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
 function App() {
-  return <div>Hello World!!!!</div>;
+  return (
+    <div className="app">
+      <div className="container">
+
+      <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
